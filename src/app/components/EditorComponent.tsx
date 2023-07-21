1f59d73c5741bc40b98c6a5bc826da0ad41e3247
RafaelSotero-dev <rafaeljesusabc2@gmail.com>
2023-07-21 13:20:45 -0300

    feat: Creating the text editor

---
+++ src/app/components/EditorComponent.tsx
@@ -0,0 +1,201 @@
+'use client'
+
+import '../styles/styles.css'
+import {
+  useEditor,
+  EditorContent,
+  BubbleMenu,
+  FloatingMenu,
+} from '@tiptap/react'
+import CodeBlockLowlight from '@tiptap/extension-code-block-lowlight'
+import StarterKit from '@tiptap/starter-kit'
+import { initialContent } from '../utils/initialContent'
+import BubbleButton from './BubbleButton'
+import { RxFontBold, RxFontItalic, RxCode } from 'react-icons/rx'
+import { lowlight } from 'lowlight'
+import typescript from 'highlight.js/lib/languages/typescript'
+import * as ToggleGroup from '@radix-ui/react-toggle-group'
+
+import 'highlight.js/styles/base16/dracula.css'
+import Image from 'next/image'
+import Placeholder from '@tiptap/extension-placeholder'
+
+lowlight.registerLanguage('typescript', typescript)
+
+const EditorComponent = () => {
+  const editor = useEditor({
+    extensions: [
+      StarterKit,
+      CodeBlockLowlight.configure({
+        lowlight,
+      }),
+      Placeholder.configure({
+        placeholder: ({ node }) => {
+          if (node.type.name === 'heading') {
+            return 'What’s the title?'
+          }
+
+          return 'Write something …'
+        },
+      }),
+    ],
+    content: initialContent,
+    editorProps: {
+      attributes: {
+        class: 'outline-none',
+      },
+    },
+  })
+
+  return (
+    <>
+      {editor && (
+        <BubbleMenu
+          editor={editor}
+          tippyOptions={{ duration: 100 }}
+          className="bg-zinc-700 shadow-xl border border-zinc-600 shadow-black/20 rounded-lg overflow-hidden flex"
+        >
+          <ToggleGroup.Root className="flex items-center" type="single">
+            <ToggleGroup.Item
+              value="bold"
+              onClick={() => editor.chain().focus().toggleBold().run()}
+            >
+              <BubbleButton data-active={editor.isActive('bold')}>
+                <RxFontBold className="w-4 h-4" />
+              </BubbleButton>
+            </ToggleGroup.Item>
+            <ToggleGroup.Item
+              value="italic"
+              onClick={() => editor.chain().focus().toggleItalic().run()}
+            >
+              <BubbleButton data-active={editor.isActive('italic')}>
+                <RxFontItalic className="w-4 h-4" />
+              </BubbleButton>
+            </ToggleGroup.Item>
+            <ToggleGroup.Item
+              value="code"
+              onClick={() => editor.chain().focus().toggleCode().run()}
+            >
+              <BubbleButton data-active={editor.isActive('code')}>
+                <RxCode className="w-4 h-4" />
+              </BubbleButton>
+            </ToggleGroup.Item>
+          </ToggleGroup.Root>
+        </BubbleMenu>
+      )}
+      {editor && (
+        <FloatingMenu
+          editor={editor}
+          tippyOptions={{ duration: 100 }}
+
+          shouldShow={({ state }) => {
+            const { $from } = state.selection
+            const currentLineText = $from.nodeBefore?.textContent
+
+            return currentLineText === '/'
+          }}
+          className="p-1 rounded-lg bg-zinc-700 shadow-xl border border-zinc-600 shadow-black/20 overflow-hidden flex flex-col"
+        >
+          <ToggleGroup.Root type="single" className="flex flex-col p-0 w-fit">
+            <ToggleGroup.Item
+              value="paragraph"
+              onClick={() => editor.chain().focus().setParagraph().run()}
+              data-active={editor.isActive('paragraph')}
+            >
+              <div className="flex items-center gap-2 h-fit p-1">
+                <Image
+                  src="https://www.notion.so/images/blocks/text/en-US.png"
+                  alt="Text"
+                  width={40}
+                  height={40}
+                  className="rounded m-0"
+                />
+                <div className="flex flex-col items-start gap-1">
+                  <span className="text-sm text-zinc-200">Text</span>
+                  <span className="text-xs text-zinc-400">
+                    Start writing with plain text
+                  </span>
+                </div>
+              </div>
+            </ToggleGroup.Item>
+
+            <ToggleGroup.Item
+              value="heading"
+              onClick={() =>
+                editor.chain().focus().toggleHeading({ level: 1 }).run()
+              }
+              data-active={editor.isActive('heading', { level: 1 })}
+            >
+              <div className="flex items-center gap-2 h-fit p-1">
+                <Image
+                  src="https://www.notion.so/images/blocks/header.57a7576a.png"
+                  alt="heading 1"
+                  width={40}
+                  height={40}
+                  className="rounded m-0"
+                />
+                <div className="flex flex-col items-start justify-center gap-1">
+                  <span className="text-md text-zinc-200">heading 1</span>
+                  <span className="text-xs text-zinc-400">
+                    large section title
+                  </span>
+                </div>
+              </div>
+            </ToggleGroup.Item>
+
+            <ToggleGroup.Item
+              value="heading 2"
+              onClick={() =>
+                editor.chain().focus().toggleHeading({ level: 2 }).run()
+              }
+              data-active={editor.isActive('heading', { level: 2 })}
+            >
+              <div className="flex items-center gap-2 h-fit p-1">
+                <Image
+                  src="https://www.notion.so/images/blocks/subheader.9aab4769.png"
+                  alt="heading 2"
+                  width={40}
+                  height={40}
+                  className="rounded m-0"
+                />
+                <div className="flex flex-col items-start justify-center gap-1">
+                  <span className="text-md text-zinc-200">heading 2</span>
+                  <span className="text-xs text-zinc-400">
+                    Medium section title
+                  </span>
+                </div>
+              </div>
+            </ToggleGroup.Item>
+
+            <ToggleGroup.Item
+              value="heading 3"
+              onClick={() =>
+                editor.chain().focus().toggleHeading({ level: 3 }).run()
+              }
+              data-active={editor.isActive('heading', { level: 3 })}
+            >
+              <div className="flex items-center gap-2 h-fit p-1">
+                <Image
+                  src="https://www.notion.so/images/blocks/subsubheader.d0ed0bb3.png"
+                  alt="heading 3"
+                  width={40}
+                  height={40}
+                  className="rounded m-0"
+                />
+                <div className="flex flex-col items-start justify-center gap-1">
+                  <span className="text-md text-zinc-200">heading 3</span>
+                  <span className="text-xs text-zinc-400">
+                    Small section title
+                  </span>
+                </div>
+              </div>
+            </ToggleGroup.Item>
+          </ToggleGroup.Root>
+        </FloatingMenu>
+      )}
+      <EditorContent editor={editor} />
+    </>
+  )
+}
+
+export default EditorComponent
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,43 @@
-import Image from 'next/image'
+import EditorComponent from './components/EditorComponent'
 
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <main className="min-h-screen flex items-start justify-center lg:p-8 bg-gradient-to-bl from-rose-900 via-amber-800 to-rose-400">
+      <section
+        className={`
+        max-sm:w-full
+        max-sm:min-h-screen
+        sm:w-full
+        sm:min-h-screen
+        sm:flex
+        sm:items-center
+        sm:pl-8
+        min-[720px]:pl-16
+        min-[840px]:pl-32
+        lg:pl-0
+        lg:max-w-[60.249rem]
+        lg:min-h-[calc(100vh-4rem)]
+        lg:grid
+        lg:grid-cols-[230px_1fr]
+        lg:rounded
+        bg-zinc-800
+        shadow-zinc-800
+        shadow-sm
+        overflow-hidden
+        `}
+      >
+        <aside className="p-4 bg-zinc-900 border-r border-zinc-900 max-sm:hidden sm:hidden lg:flex lg:h-full items-start">
+          <div className="flex items-center gap-2 group w-fit">
+            <button className="w-3 h-3 border-none bg-zinc-600 group-hover:bg-red-400 rounded-[100%]" />
+            <button className="w-3 h-3 border-none bg-zinc-600 group-hover:bg-yellow-400 rounded-[100%]" />
+            <button className="w-3 h-3 border-none bg-zinc-600 group-hover:bg-emerald-400 rounded-[100%]" />
+          </div>
+        </aside>
+
+        <div className="p-8 prose prose-invert">
+          <EditorComponent />
+        </div>
+      </section>
+    </main>
   )
 }
